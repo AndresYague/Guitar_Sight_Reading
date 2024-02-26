5aedb422dfb04281ea29af557cc2afdae398775e
Andrés Yagüe López <and.yague@gmail.com>
2024-02-25 20:17:31 -0700

    Created function to tell if the desired frequency is playing

--- guitar_trainer.py
+++ guitar_trainer.py
@@ -2,34 +2,95 @@ import pyaudio
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-import scipy.fft as fft
 
-def record_stream(record_seconds=5):
+def find_frequency(freq, n_consecutive=5, record_seconds=5, chunk=1024,
+                   chunkps=8):
     '''
-    Open a record stream with pyaudio
+    Keep recording until freq is found a n_consecutive number of times,
+    consecutively
+
+    record_seconds is the maximum record time
+    chunk is the number of byte strings to get
+    chunksps is how many chunks in a seconds to record
     '''
 
-    chunk = 1024
+    # Define the tolerance of quarter tone
+    tol_p = 2 ** (1 / 24)
+    tol_m = 2 ** (-1 / 24)
+
+    # Take 32 bits for high amplitudes
     format_ = pyaudio.paInt32
     channels = 1 if sys.platform == 'darwin' else 2
-    rate = 44100
-    rate *= 2
-    record_seconds = 5
 
+    # Calculate the byte rate
+    rate = chunkps * chunk
+
+    # Open the stream
     p = pyaudio.PyAudio()
     stream = p.open(format=format_, channels=channels, rate=rate, input=True)
 
-    amplitude = []
-    for _ in range(0, rate // chunk * record_seconds):
-        value = stream.read(chunk)
-        value = np.fromstring(value, np.int32)
-        amplitude.append(value)
-    amplitude = np.ndarray.flatten(np.array(amplitude))
+    # Variables for fft
+    time = 1 / chunkps
+    total_time = 0
+    dt = None
+
+    # Record
+    found_consecutive = 0
+    did_found = False
+    while True:
+
+        # Read the amplitude in byte strings
+        amplitude = stream.read(chunk)
+
+        # Transform to int32
+        amplitude = np.fromstring(amplitude, np.int32)
+
+        # Define dt if not defined already
+        if dt is None:
+            dt = time / amplitude.shape[0]
+            frequency = np.fft.fftfreq(amplitude.shape[0], d=dt)
+
+        # Perform the fft and take the norm of it
+        spectrum = np.fft.fft(amplitude)
+        spectrum_norm = np.sqrt(spectrum.real ** 2 + spectrum.imag ** 2)
+
+        # Find the index of the highest frequency component
+        index = np.argmax(spectrum_norm)
+
+        # Retrieve the values associated with the index
+        max_freq = np.abs(frequency[index])
+        max_spec = spectrum_norm[index]
+        s = f"{max_freq:.2f} {max_spec:.2e}"
+        print(s)
+
+        # Check the frequency ratio (or the next octave)
+        ratio = max_freq / freq
+        ov_ratio = max_freq / (2 * freq)
+
+        # Check if the ratio is within the tolerances
+        comp1 = ratio > tol_m and ratio < tol_p
+        comp2 = ov_ratio > tol_m and ov_ratio < tol_p
+
+        if comp1 or comp2:
+            found_consecutive += 1
+        else:
+            found_consecutive = 0
+
+        # If found enough consecutives, we can assume the frequency
+        # was being played
+        if found_consecutive >= n_consecutive:
+            did_found = True
+            break
+
+        # If time exceeds recording time, break
+        total_time += time
+        if total_time > record_seconds:
+            break
 
     stream.close()
     p.terminate()
 
-    time = np.linspace(0, record_seconds, amplitude.shape[0], endpoint=True)
+    return did_found
 
     return time, amplitude
 
@@ -38,15 +99,11 @@ def main():
     Train playing the guitar from sheet music
     '''
 
-    time, amplitude = record_stream(record_seconds=2)
+    record_seconds = 10
 
-    spectrum = fft.fft(amplitude)
-    frequency = fft.fftfreq(amplitude.shape[0], d=(time[1] - time[0]))
-    plt.plot(frequency, spectrum)
-    plt.xlim([0, 1000])
-    plt.show()
-
-    pass
+    halftones = -14
+    is_found = find_frequency(note_freq(halftones), n_consecutive=5,
+                              record_seconds=record_seconds)
 
 if __name__ == "__main__":
     main()
